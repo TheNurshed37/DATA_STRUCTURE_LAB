7e5545a7152a11c2007bfe6585e1be6f0da1c3b9
Nur-E Habib Nurshed <114690567+TheNurshed37@users.noreply.github.com>
2022-10-08 10:57:42 +0600

    Update LT3C.cpp

--- LT3(22-46702-1)/LT3C.cpp
+++ LT3(22-46702-1)/LT3C.cpp
@@ -8,19 +8,15 @@ struct Employ{
 };
 int main(){
 	int a=10;
-   // struct Employ record[10];
     struct Employ info[a];
-    for(int i=0;i<a;i++){
-        //cout<<"Enter ID:";
+    for(int i=0;i<a;i++)
+    {
         cin>>info[i].id;
-        //cout<<"Enter Deptname:";
         cin>>info[i].daptname;
-        //cout<<"Enter Salae:";
         cin>>info[i].sal;
         //cout<<"Enter Join Year:";
         cin>>info[i].joinyear;
     }
-
     for(int i=0;i<a;i++){
         if(info[i].sal>20000 && info[i].joinyear<2009){
             cout<<info[i].id<<endl;
